3ffc230d7535bebadbc1ae009449b2b930a417c0
Manish Shankla <manish@Manishs-MacBook-Pro-2.local>
2020-10-18 14:08:43 -0700

    .

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,9 +1,10 @@
 import axios from 'axios';
 import { LOGOUT } from '../actions/types';
 import store from '../store';
+//	baseURL: 'http://localhost:5000/api',
 
 const api = axios.create({
-	baseURL: 'http://localhost:5000/api',
+	baseURL: '/api',
 	headers: {
 		'Content-Type': 'application/json',
 	},
