e6c439793b10299efd1890dbf48a6b7f592b57bb
Manish Shankla <manish@Manishs-MacBook-Pro-2.local>
2020-10-18 21:26:11 -0700

    new runscript

--- run.sh
+++ run.sh
@@ -11,5 +11,12 @@ cd ..
 #install python devs
 pip install -r requirements.txt
 
+#copy over correct binary
+if [ "$1" == "linux" ]; then
+    cp alignment/bin/blastn_linux alignment/blastn 
+else
+    cp alignment/bin/blastn_mac alignment/blastn 
+fi
+
 #run the app locally
 npm run dev 
